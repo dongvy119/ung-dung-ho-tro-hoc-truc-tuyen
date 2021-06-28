26c50c5f96df8d803c80acfdab0f03e3fb2df61d
dongvy119 <dongvy119@gmail.com>
2021-06-28 15:11:48 +0700

    Update centos8.sh

--- centos8.sh
+++ centos8.sh
@@ -55,7 +55,6 @@ setuid 65535
 stacksize 6291456 
 flush
 auth iponly
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 $(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
