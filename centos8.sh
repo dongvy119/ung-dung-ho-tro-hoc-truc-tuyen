0956471759cd3ced29f23a382034465857da6c27
dongvy119 <dongvy119@gmail.com>
2021-06-28 14:08:48 +0700

    Update centos8.sh

--- centos8.sh
+++ centos8.sh
@@ -55,7 +55,8 @@ setuid 65535
 stacksize 6291456 
 flush
 auth iponly
-$(awk -F "/" '{print "auth none\n" \
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
+$(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
@@ -64,7 +65,7 @@ EOF
 
 gen_proxy_file_for_user() {
     cat >proxy.txt <<EOF
-$(awk -F "/" '{print $3 ":" $4}' ${WORKDATA})
+$(awk -F "/" '{print $3 ":" $4 }' ${WORKDATA})
 EOF
 }
 
