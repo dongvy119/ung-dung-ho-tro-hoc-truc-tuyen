1a07e047eb6355f2beb77f8965633727ff4e1600
dongvy119 <dongvy119@gmail.com>
2021-06-28 15:36:34 +0700

    Update centos8.sh

--- centos8.sh
+++ centos8.sh
@@ -52,10 +52,9 @@ nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
-stacksize 6291456 
 flush
 auth iponly
-$(awk -F "/" '{print "auth strong\n" \
+$(awk -F "/" '{print "auth none\n" \
 "allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
